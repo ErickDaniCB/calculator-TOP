fa1cbed59f3d492c5b68fe2c639f0d4e5b1a92f9
ErickDaniCB <erickdani10@hotmail.com>
2023-08-10 13:24:23 -0600

    Add function and display interaction to each number and clear button

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
-function add (){
-    
-}
+const numbers = document.querySelectorAll('.number');
+const display = document.querySelector('#display');
+const clear = document.querySelector('#clear');
+const equals = document.querySelector('#equals');
+
+let tempArr = [];
+
+numbers.forEach(button => button.addEventListener('click', () => {
+    tempArr.push(parseInt(button.textContent));
+    console.log(tempArr);
+    display.textContent += button.textContent;
+}));
+
+clear.addEventListener('click', () => {
+    tempArr = [];
+    display.textContent = '';
+});
+
