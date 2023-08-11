efce5b6b660b33b881b815299162c3a5e7c49c6c
Dandelionstorm6 <62414624+Dandelionstorm6@users.noreply.github.com>
2023-08-10 22:57:22 -0600

    1.0 Operations
    Logic in how to distinguish operations and display the result

--- calculator.js
+++ calculator.js
@@ -10,6 +10,7 @@ let tempNum;
 
 let arrNum = [];
 let arrOp = [];
+let op= 0;
 
 //calculation - number
 numbers.forEach(button => button.addEventListener('click', () => {
@@ -21,15 +22,57 @@ numbers.forEach(button => button.addEventListener('click', () => {
 digit.forEach(button => button.addEventListener('click', () => {
     console.log(button.classList[1]);
     if(button.classList[1] === 'number'){
-        arrNum.push(button.value);
+
+        if(arrOp.length == 0){
+            if(arrNum.length == 0){
+                arrNum.push(button.value);
+            }else{
+                arrNum[0] = arrNum[0]+button.value;
+                console.log(arrNum[0]);
+            }
+        }else{
+            if(arrNum.length == 1){
+                arrNum.push(button.value); 
+            }else{
+                arrNum[1] = arrNum[1]+button.value;
+                console.log(arrNum[1]);
+            }
+            
+        }
+        
+        
+        display.textContent += button.value;
     }
     else {
-        arrOp.push(button.value);
+        if(arrOp.length < 1){
+            arrOp.push(button.value);
+            display.textContent += button.value;
+        }else{
+           if(arrNum.length > 1 ){
+                if(button.classList[1] === 'equals'){
+                    display.textContent = operations[arrOp[0]](parseInt(arrNum[0]),parseInt(arrNum[1]));
+                    arrNum[0] = display.textContent;
+                    arrNum[1] = 0;
+                    arrOp = [];
+                }else{
+                    display.textContent = operations[arrOp[0]](parseInt(arrNum[0]),parseInt(arrNum[1]));
+                    arrNum[0] = display.textContent;
+                    arrNum[1] = 0;
+                    arrOp[0] = (button.value);
+                    display.textContent += button.value;
+                }
+                
+                
+
+           }
+        }
     }
 }));
 
 clear.addEventListener('click', () => {
-    tempArr = [];
+    arrNum = [];
+    arrOp = [];
+    
     display.textContent = '';
 });
 
