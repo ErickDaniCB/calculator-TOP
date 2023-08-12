3b999c91cc1e8b1947e2d6e5ce6b8ee58e003afb
ErickDaniCB <erickdani10@hotmail.com>
2023-08-11 18:18:54 -0600

    Implement calculator logic

--- calculator.js
+++ calculator.js
@@ -1,46 +1,56 @@
 const digit = document.querySelectorAll('.digit');
 const numbers = document.querySelectorAll('.number');
-const display = document.querySelector('#display');
+const operation = document.querySelector('#operation');
+const disResult = document.querySelector('#result');
 const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
 const operators = document.querySelectorAll('.operator');
-
-let tempArr = [];
-let tempNum;
-
-let arrNum = [];
-let arrOp = [];
-
-//calculation - number
-numbers.forEach(button => button.addEventListener('click', () => {
-    tempArr.push(parseInt(button.textContent));
-    tempNum = parseInt(tempArr.join(''));
-}));
-
-//display - string
-digit.forEach(button => button.addEventListener('click', () => {
-    console.log(button.classList[1]);
-    if(button.classList[1] === 'number'){
-        arrNum.push(button.value);
-    }
-    else {
-        arrOp.push(button.value);
-    }
-}));
-
-clear.addEventListener('click', () => {
-    tempArr = [];
-    display.textContent = '';
-});
-
-let currentOp = operators.forEach(operator => operator.addEventListener('click', () => {
-    return operator.value;
-}));
-
 const operations = { 
     '+': function(a,b){return a + b},
     '-': function(a,b){return a - b},
     '*': function(a,b){return a * b},
     '/': function(a,b){return a / b},
 };
+
+let numTemp = [];
+let arrOperation = [];
+let operatorDig;
+let result
+
+digit.forEach(button => button.addEventListener('click', () => {
+    if(button.classList[1] === 'number'){
+        numTemp.push(button.value);
+        operation.textContent += button.value;
+    } else if (numTemp.length !== 0){
+        arrOperation.push(numTemp.join('')/1);
+        numTemp = [];
+        console.log(arrOperation);
+        if (arrOperation.length === 2){
+            result = operations[operatorDig](arrOperation[0],arrOperation[1]);
+            arrOperation.push(result);
+            disResult.textContent = arrOperation[2];
+            arrOperation.splice(0,2);
+        }
+        operatorDig = button.value;
+        operation.textContent += button.value;
+    }
+}));
+
+clear.addEventListener('click', () => {
+    numTemp = [];
+    arrOperation = [];
+    operation.textContent = '';
+    result = undefined;
+    disResult.textContent = '';
+});
+
+equals.addEventListener('click', () => {
+    arrOperation.push(numTemp.join('')/1);
+    numTemp = [];
+    result = operations[operatorDig](arrOperation[0],arrOperation[1]);
+    arrOperation.push(result);
+    disResult.textContent = arrOperation[2];
+    arrOperation.splice(0,2);
+    console.log(arrOperation);
+});
 
